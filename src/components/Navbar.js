f43dbfc73893446d012f1a6187e3550b2e4b4cc6
Marta Pino <165914600+Martamariavittoria@users.noreply.github.com>
2025-03-20 18:34:38 +0100

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+function CustomNavbar(){
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">EpicBooks</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+console.log("Rendering CustomNavbar...");
+
+export default CustomNavbar;
